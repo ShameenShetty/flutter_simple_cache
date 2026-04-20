c23dda5248b3d0d2eff16398ef282d8051a11678
ShameenShetty <57455717+ShameenShetty@users.noreply.github.com>
2026-04-20 10:03:59 +0530

    Added basic functionality

--- lib/flutter_simple_cache.dart
+++ lib/flutter_simple_cache.dart
@@ -1,7 +1,30 @@
 class FlutterSimpleCache {
+  static FlutterSimpleCache? _instance;
+  static final Map<String, dynamic> _cache = {};
   FlutterSimpleCache._internal();
 
-  static final FlutterSimpleCache _instance = FlutterSimpleCache._internal();
+  static void init() {
+    _instance ??= FlutterSimpleCache._internal();
 
-  static FlutterSimpleCache get instance => _instance;
+    // TODO add config options
+  }
+
+  static FlutterSimpleCache get instance {
+    if (_instance == null) {
+      throw Exception("FlutterSimpleCache not initialized, call init()");
+    }
+
+    return _instance!;
+  }
+
+  static void set(String key, dynamic value) => _cache[key] = value;
+  static dynamic get(String key) => _cache[key];
+
+  static bool contains(String key) => _cache.containsKey(key);
+
+  static void remove(String key) => _cache.remove(key);
+  static void clear() => _cache.clear();
+
+  /// Returns the number of items in the cache.
+  static int get size => _cache.length;
 }
